afe617232ff717416cb654615457e491b8b3c6ba
saltmurai <phamsang6260@gmail.com>
2023-05-16 00:55:36 +0700

    feat: tweak with daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
+	theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -13,6 +13,9 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
-  },
+	},
+	daisyui: {
+		themes: ["lofi"]
+	},
   plugins: [require('daisyui')],
 }
